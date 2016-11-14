988cfec4c283e90b1ceb7c1697db82c18bbe1c29
likangbin <1218773641@qq.com>
2016-11-14 17:00:11 +0800

    都加上

--- dgtle/js/index.js
+++ dgtle/js/index.js
@@ -3,7 +3,7 @@ var func=function (i) {
             // $('slider-item')[i-1].attr('color','blue');
     // $('.slider-item').eq(i).css('color','red')
     //由红色变成蓝色
-    console.log($('.slider-item').eq(i-1).css('color','blue'));
+    $('.slider-item').eq(i-1).css('color','blue');
     //有了新的蓝色,旧的蓝色得变回红色
       //不处理,可以越界
         $('.slider-item').eq(i-2).css('color','red');
@@ -18,4 +18,18 @@ setInterval(function () {
         d=1;
     }
 } ,1000);
-
+//
+// var xmlhttp;
+// if (window.XMLHttpRequest)
+// {// code for IE7+, Firefox, Chrome, Opera, Safari
+//     xmlhttp=new XMLHttpRequest();
+// }
+// else
+// {// code for IE6, IE5
+//     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+// }
+//数据
+// var  str='http://www.dgtle.com/api/dgtle_api/v1/api.php?charset=UTF8&dataform=json&swh=480x800&apikeys=DGTLECOM_APITEST1&modules=portal&actions=diydata&bid=274';
+// var array=xmlhttp.open("GET",str,true);
+// xmlhttp.send();
+// console.log(array);
