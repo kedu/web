ca16b52add8e8c189c99b6030d5b0fc464e7a1a1
likangbin <1218773641@qq.com>
2016-11-16 12:04:05 +0800

    连接数据库,查询数据

--- test-api.php
+++ test-api.php
@@ -1,46 +1,33 @@
 <?php
 //新建数组用户保存输出
-$output = array();
-//三目运算符
-$a = @$_GET['a'] ? $_GET['a'] : '';
-$uid = @$_GET['uid'] ? $_GET['uid'] : 0;
-//这个是get方法
-if (empty($a)) {
-    $output = array('data'=>NULL, 'info'=>'坑爹啊!', 'code'=>-201);
-//    转为json输出
-    exit(json_encode($output));
+//$output = array();
+////三目运算符
+//$a = @$_GET['a'] ? $_GET['a'] : '';
+//$uid = @$_GET['uid'] ? $_GET['uid'] : 0;
+////这个是get方法
+//if (empty($a)) {
+//    $output = array('data'=>NULL, 'info'=>'坑爹啊!', 'code'=>-201);
+////    转为json输出
+////    exit(json_encode($output));
+//    exit(json_encode($output));
+//}
+$name = $_POST["name"];
+//$sex  = $_POST["sex"];
+$name="lisi";
+
+// 连接数据库的函数,后边的三个参数分别是数据库地址、数据库账号、密码
+$link = mysqli_connect("likangbin.top","a1112210156","1664a59a","a1112210156");
+// 判断链接是否成功
+if ($link) {
+    echo("链接成功\n");
+} else {
+    echo("连接失败\n");
 }
-$output=array('data'=>'传的参数不为空','info'=>'你要发大财了','code'=>'200');
-//exit(json_encode($output));
-$type = $_POST['type'];
-$data = '';
-//是否为空,是不是数字,必须大于0
-if (isset($type) && is_numeric($type) && $type >= 0) {
-    if ($type == 1) {
-        $data = array(
-            'type' => $type,
-            'name' => '标哥的技术博客',
-            'site' => 'www.huangyibiao.com');
-    } else if ($type == 2) {
-        $data = array(
-            'type' => $type,
-            'name' => '公众号：标哥的技术博客',
-            'site' => 'weixin search: biaogedejishuboke');
-    }
-
-    $response = array(
-        'code'    => 200,
-        'message' => 'success for request',
-        'data'    => $data,
-    );
-
-    exit(json_encode($response));
-}
-
-$response = array(
-    'code'    => 999,
-    'message' => 'argument error for request',
-    'data'    => $data,
-);
-
-exit(json_encode($response));
+$sql="SELECT * FROM test";
+echo $sql;
+$data=mysqli_query($link,$sql);
+echo ("\n");
+$result=$data->fetch_assoc();
+echo $result['name'];
+$result=$data->fetch_assoc();
+echo $result['name'];
